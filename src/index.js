516959d6faa8e847e4aaf667f111bd900ab08c3f
Jeremy <jeremy.ling@unotech.io>
2021-07-29 09:57:49 +0800

    export all components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import FormBuilder from "./Components/FormBuilder";
+import StandardAutocomplete from "./Components/Forms/StandardAutocomplete";
+import StandardDatePicker from "./Components/Forms/StandardDatePicker";
+import StandardDateTimePicker from "./Components/Forms/StandardDateTimePicker";
+import StandardSelect from "./Components/Forms/StandardSelect";
+import StandardTextField from "./Components/Forms/StandardTextField";
 
+export {
+  StandardAutocomplete,
+  StandardDatePicker,
+  StandardDateTimePicker,
+  StandardSelect,
+  StandardTextField,
+};
 export default FormBuilder;
