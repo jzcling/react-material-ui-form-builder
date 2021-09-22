fef7f5a83e0684eba5ac0b223e36d80463420b6a
Jeremy <jlzc89@gmail.com>
2021-09-23 01:31:49 +0800

    allow referencing other fields in title

--- src/Components/Widgets/Title.js
+++ src/Components/Widgets/Title.js
@@ -1,12 +1,35 @@
 import { Typography } from "@material-ui/core";
-import React, { forwardRef } from "react";
+import React, { forwardRef, useMemo } from "react";
 import PropTypes from "prop-types";
+import get from "lodash/get";
 
 const Title = forwardRef((props, ref) => {
-  const { field } = props;
+  const { field, form } = props;
+
+  const title = useMemo(() => {
+    if (field.formula) {
+      var sub = field.formula.replace(/@\w*/g, (match) =>
+        parseFloat(get(form, match.replace("@", "")))
+      );
+      try {
+        return eval(sub) || "";
+      } catch (error) {
+        return "";
+      }
+    }
+    sub = field.title.replace(/@\w*/g, (match) =>
+      get(form, match.replace("@", ""))
+    );
+    try {
+      return sub || "";
+    } catch (error) {
+      return field.title || "";
+    }
+  }, [field.formula, field.title, form]);
+
   return (
     <div ref={ref} style={{ display: "flex", alignItems: "baseline" }}>
-      <Typography {...field.titleProps}>{field.title}</Typography>
+      <Typography {...field.titleProps}>{title}</Typography>
       <div style={{ width: "2px" }} />
       {field.titleSuffixComponent
         ? field.titleSuffixComponent
@@ -23,6 +46,7 @@ Title.displayName = "Title";
 
 Title.propTypes = {
   field: PropTypes.object,
+  form: PropTypes.object,
 };
 
 export { Title };
