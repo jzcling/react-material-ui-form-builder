179f6aa67a848dd929c2ee82752b871e7a5394dd
Jeremy <jlzc89@gmail.com>
2021-11-02 22:05:08 +0800

    minor change

--- rollup.config.js
+++ rollup.config.js
@@ -135,22 +135,8 @@ const config = [
           ],
           "babel-plugin-lodash",
           [
-            "babel-plugin-import",
-            {
-              libraryName: "@mui/material",
-              libraryDirectory: "",
-              camel2DashComponentName: false,
-            },
-            "core",
-          ],
-          [
-            "babel-plugin-import",
-            {
-              libraryName: "@mui/icons-material",
-              libraryDirectory: "",
-              camel2DashComponentName: false,
-            },
-            "icons",
+            "babel-plugin-direct-import",
+            { modules: ["@mui/material", "@mui/icons-material"] },
           ],
         ],
         presets: ["@babel/react", "@babel/env"],
