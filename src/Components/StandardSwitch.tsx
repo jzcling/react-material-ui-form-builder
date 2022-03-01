fcc33245e428f65dd7b93c5361f9149ea97d75aa
Jeremy <jlzc89@gmail.com>
2022-03-01 10:23:00 +0800

    refactor switch

---
+++ src/Components/StandardSwitch.tsx
@@ -0,0 +1,84 @@
+import React, { forwardRef, Fragment } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+
+import { FormControl, FormControlLabel, FormHelperText, Switch, SwitchProps } from "@mui/material";
+
+import { getTitleProps } from "../utils";
+import { CommonFieldProps, SwitchFieldProps } from "./props/FieldProps";
+import { Title, TitleProps } from "./widgets/Title";
+
+export interface StandardSwitchProps
+  extends CommonFieldProps,
+    SwitchFieldProps {
+  attribute: Required<CommonFieldProps>["attribute"];
+  label: Required<CommonFieldProps>["label"];
+  props: SwitchProps;
+}
+
+const StandardSwitch = forwardRef(
+  (props: { field: StandardSwitchProps; showTitle: boolean }, ref) => {
+    const {
+      control,
+      getValues,
+      setValue,
+      trigger,
+      formState: { errors },
+    } = useFormContext();
+    const { field: fieldConfig, showTitle } = props;
+    const titleProps: TitleProps = getTitleProps(fieldConfig);
+
+    const handleSwitchChange = (checked: boolean, value?: unknown) => {
+      if (checked) {
+        setValue(fieldConfig.attribute, value);
+      } else {
+        setValue(fieldConfig.attribute, undefined);
+      }
+    };
+
+    const componentProps = (
+      fieldConfig: StandardSwitchProps,
+      value?: unknown
+    ): SwitchProps => {
+      return {
+        id: fieldConfig.attribute,
+        key: fieldConfig.attribute,
+        size: "small",
+        color: "primary",
+        checked: !!value,
+        onChange: (event) => handleSwitchChange(event.target.checked, value),
+        onBlur: () => trigger(fieldConfig.attribute),
+        ...fieldConfig.props,
+      };
+    };
+
+    return (
+      <Controller
+        name={fieldConfig.attribute}
+        control={control}
+        defaultValue={getValues(fieldConfig.attribute) || false}
+        render={({ field }) => (
+          <Fragment>
+            {showTitle && titleProps.title && <Title {...titleProps} />}
+            <FormControl error={!!errors[fieldConfig.attribute]}>
+              <FormControlLabel
+                inputRef={ref}
+                key={fieldConfig.attribute}
+                control={
+                  <Switch {...componentProps(fieldConfig, field.value)} />
+                }
+                label={fieldConfig.label}
+                sx={{ margin: "8px 0" }}
+                {...fieldConfig.labelProps}
+              />
+              <FormHelperText>
+                {errors[fieldConfig.attribute]?.message}
+              </FormHelperText>
+            </FormControl>
+          </Fragment>
+        )}
+      />
+    );
+  }
+);
+
+export { StandardSwitch };
